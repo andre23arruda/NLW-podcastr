870d8d3755e823233199a9f07967fea215dcb4c6
andre23arruda <andre23arruda@gmail.com>
2021-04-21 01:46:40 -0300

    create player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,11 +3,46 @@ import styles from './styles.module.scss'
 
 function Player() {
     return (
-        <header className={ styles.headerContainer }>
-            <img src="/logo.svg" alt="Podcastr"/>
-            <p>O melhor para você ouvir, sempre</p>
-            <span>{ }</span>
-        </header>
+        <div className={ styles.playerContainer }>
+            <header>
+                <img src="/playing.svg" alt="Tocando agora"/>
+                <strong>Tocando agora</strong>
+            </header>
+
+            <div className={ styles.emptyPlayer }>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer className={ styles.empty }>
+                <div className={ styles.progress }>
+                    <span>00:00</span>
+                    <div className={ styles.slider }>
+                        <div className={ styles.emptySlider }></div>
+                    </div>
+                    <span>00:00</span>
+
+                </div>
+
+                <div className={ styles.buttons }>
+                    <button>
+                        <img src="/shuffle.svg" alt="Random"/>
+                    </button>
+                    <button>
+                        <img src="/play-previous.svg" alt="Back"/>
+                    </button>
+                    <button className={ styles.playButton }>
+                        <img src="/play.svg" alt="Play"/>
+                    </button>
+                    <button>
+                        <img src="/play-next.svg" alt="Next"/>
+                    </button>
+                    <button>
+                        <img src="/repeat.svg" alt="Repeat"/>
+                    </button>
+                </div>
+
+            </footer>
+        </div>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <div>
-      <Header></Header>
+      Index
+
     </div>
   )
 }
