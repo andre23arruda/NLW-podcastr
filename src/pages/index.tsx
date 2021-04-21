b5e7c98057901de832dd9f3d67c6b87491063afc
andre23arruda <andre23arruda@gmail.com>
2021-04-21 02:17:51 -0300

    Add static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,45 @@
-import Header from "../components/Header";
+import { useEffect } from "react"
 
-export default function Home() {
+
+export default function Home(props) {
+  console.log(props.episodes)
+
+  //// SPA
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //    .then(response => response.json())
+  //    .then(data => console.log(data))
+  // }, [])
+
   return (
     <div>
       Index
 
     </div>
   )
+}
+
+//// SSR
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data
+//     }
+//   }
+// }
+
+//// SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
