d42cc252dd8d461ebe995a8c2e27c3d0d736794f
andre23arruda <andre23arruda@gmail.com>
2021-04-23 01:20:37 -0300

    add slider

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import { useContext } from 'react'
 import PlayerContext from '../../contexts/PlayerContext'
 import styles from './styles.module.scss'
+import Slider from 'rc-slider'
+
+import 'rc-slider/assets/index.css'
 
 function Player() {
 
@@ -18,7 +21,7 @@ function Player() {
             { episode
                 ?(
                     <div className={ styles.currentEpisode }>
-                        <Image width={ 592 } height={ 592 } src={ episode.thumbnail } alt={ episode.title } objectFit="initial"></Image>
+                        <Image width={ 592 } height={ 592 } src={ episode.thumbnail } alt={ episode.title } objectFit="cover"></Image>
                         <strong>{ episode.title }</strong>
                         <span>{ episode.members }</span>
                     </div>
@@ -34,27 +37,36 @@ function Player() {
             <footer className={ !episode && styles.empty }>
                 <div className={ styles.progress }>
                     <span>00:00</span>
-                    <div className={ styles.slider }>
-                        <div className={ styles.emptySlider }></div>
-                    </div>
+
+                    { episode
+                        ?(
+                            <Slider></Slider>
+                        )
+                        :(
+                            <div className={ styles.slider }>
+                                <div className={ styles.emptySlider }></div>
+                            </div>
+                        )
+                    }
+
                     <span>00:00</span>
 
                 </div>
 
                 <div className={ styles.buttons }>
-                    <button>
+                    <button disabled={ !episode }>
                         <img src="/shuffle.svg" alt="Random"/>
                     </button>
-                    <button>
+                    <button disabled={ !episode }>
                         <img src="/play-previous.svg" alt="Back"/>
                     </button>
                     <button className={ styles.playButton }>
                         <img src="/play.svg" alt="Play"/>
                     </button>
-                    <button>
+                    <button disabled={ !episode }>
                         <img src="/play-next.svg" alt="Next"/>
                     </button>
-                    <button>
+                    <button disabled={ !episode }>
                         <img src="/repeat.svg" alt="Repeat"/>
                     </button>
                 </div>
